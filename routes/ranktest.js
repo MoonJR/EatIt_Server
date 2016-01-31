b0b033bfa998eca345209d5a3b033f3cf3185af8
MoonJongRak <mox7050@gmail.com>
2016-01-31 23:05:19 +0900

    test

--- routes/ranktest.js
+++ routes/ranktest.js
@@ -7,34 +7,11 @@ var connection = mysql.createConnection({
     'password': 'eatit123',
     'database': 'eat_it',
 });
+
 router.post('/', function (req, res, next) {
 
     var jsontext = JSON.stringify(req.body[0]);
     var contact = JSON.parse(jsontext);
-
-    console.log(contact);
-
-
-    var query = 'UPDATE my_info ' +
-        'SET my_info.my_weight=CASE ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
-        'END ' +
-        'WHERE my_info.my_food_index IN(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) AND my_info.user_id=?';
 
     connection.query('select * from user_info where user_id= ?  and food_index=? ;', [contact.user_id, contact.food_index], function (error, info1) {
         if (error == null) {
@@ -49,27 +26,15 @@ router.post('/', function (req, res, next) {
                         queryArray.push(con.weight);
                         queryArrayTmp.push(con.food_index);
                     }
-                    if (con.weight > 0) {
 
-                        connection.query('UPDATE user_info SET weight = weight + ?  WHERE user_id = ? and food_index = ? ; ', [con.weight, con.user_id, con.food_index], function (error2, info2) {
-                            if (error2 != null) {
-                                console.log(error2);
-                                res.status(503).json(error);
-                            }
-                        });
-
-                        connection.query('UPDATE food_list SET rank = rank + ? WHERE food_index = ? ; ', [con.weight, con.food_index], function (error2, info2) {
-                            if (error2 != null) {
-                                console.log(error2);
-                                res.status(503).json(error);
-                            }
-                        });
-                    }
                 }
 
                 queryArray = queryArray.concat(queryArrayTmp);
                 queryArray.push(contact.user_id);
-                connection.query(query, queryArray, function (error2, info2) {
+
+                queryArray = queryArray.concat(queryArray).concat(queryArray);
+
+                connection.query(query_my_info + query_user_info + query_food_list, queryArray, function (error2, info2) {
                     if (error2 != null) {
                         console.log(error2);
                         res.status(503).json(error);
@@ -115,4 +80,66 @@ router.post('/', function (req, res, next) {
         }
     });
 });
+
+
+var query_my_info = 'UPDATE my_info ' +
+    'SET my_info.my_weight=CASE ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+    'END ' +
+    'WHERE my_info.my_food_index IN(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) AND my_info.user_id=?;';
+var query_user_info = 'UPDATE my_info ' +
+    'SET user_info.weight=CASE ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'WHEN user_info.food_index=? THEN user_info.weight+? ' +
+    'END ' +
+    'WHERE user_info.food_index IN(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) AND user_info.user_id=?;';
+var query_food_list = 'UPDATE food_list ' +
+    'SET food_list.rank=CASE ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'WHEN food_list.food_index=? THEN food_list.rank+? ' +
+    'END ' +
+    'WHERE food_list.food_index IN(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
 module.exports = router;
