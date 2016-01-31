6e80579854b6676909dba9a60c3982196d63764e
MoonJongRak <mox7050@gmail.com>
2016-01-31 18:56:05 +0900

    test

--- routes/allrank.js
+++ routes/allrank.js
@@ -11,21 +11,6 @@ var connection = mysql.createConnection({
 });
 
 
-//var dir = ["아메리카노", "블루베리 빙수", "브라우니", "버블티", "카페라떼", "과일케이크", "사탕", "치즈타르트", "초코빙수", "블루베리 타르트", "카나페", "초콜릿",
-//    "츄러스", "쿠키", "솜사탕", "도넛", "에그타르트", "생과일주스", "과일차", "녹차라떼", "허니브레드", "아이스크림", "카놀리", "치즈케이크", "크레페", "젤리", "레몬머랭파이", "마카롱",
-//    "망고빙수", "마쉬멜로우", "멜론빙수", "머핀", "오레오빙수", "팬케이크", "판나코타", "빼빼로", "파이", "감자칩", "프레첼", "푸딩", "팥죽", "떡", "롤케이크", "쉐이크", "슈크림",
-//    "스무디", "딸기타르트", "딸기빙수", "고구마", "탕후루", "차", "티라미수", "와플", "약과", "요거트", "붕어빵", "자판기 커피", "데자와", "스콘", "핫도그", "새우깡", "닭강정", "감자튀김", "호떡"];
-//
-//for (var i = 0; i < dir.length; i++) {
-//    connection.query('insert into food_list values(?,?,?,?)', [dir[i], i, 1, i], function (err, result) {
-//        if (err)
-//            console.log(err);
-//        else
-//            console.log(dir[i]);
-//    });
-//}
-
-
 router.get('/:content_id', function (req, res, next) {
     connection.query('select food_name, rank, food_index from food_list where distinction = ? ' + 'order by rank desc limit 10;', [req.params.content_id], function (error, cursor) {
         if (cursor.length > 0) res.json(cursor);
--- routes/ranktest.js
+++ routes/ranktest.js
@@ -9,37 +9,56 @@ var connection = mysql.createConnection({
 });
 router.post('/', function (req, res, next) {
 
-    try {
-        var jsontext = JSON.stringify(req.body[0]);
-        var contact = JSON.parse(jsontext);
+    var jsontext = JSON.stringify(req.body[0]);
+    var contact = JSON.parse(jsontext);
 
-        console.log(contact);
+    console.log(contact);
 
-        console.log(req.body);
 
-        connection.query('select * from user_info where user_id= ?  and food_index=? and distinction = ?;', [contact.user_id, contact.food_index, contact.distinction], function (error, info1) {
-            if (error == null) {
-                if (info1.length > 0) {
-                    for (var i = 0; i < 64; i++) {  //for문시작
-                        var js = JSON.stringify(req.body[i]);
-                        var con = JSON.parse(js);
+    var query = 'UPDATE my_info ' +
+        'SET my_info.my_weight=CASE ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'WHEN my_info.my_food_index=? THEN my_info.my_weight+? ' +
+        'END ' +
+        'WHERE my_info.my_food_index IN(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) AND my_info.user_id=?';
 
-                        connection.query('UPDATE user_info SET weight = weight + ?  WHERE user_id = ? and food_index = ? and distinction = ? ; ', [con.weight, con.user_id, con.food_index, con.distinction], function (error2, info2) {
+    connection.query('select * from user_info where user_id= ?  and food_index=? and distinction = ?;', [contact.user_id, contact.food_index], function (error, info1) {
+        if (error == null) {
+            if (info1.length > 0) {
+                var queryArray = [];
+                var queryArrayTmp = [];
+                for (var i = 0; i < 64; i++) {  //for문시작
+                    var js = JSON.stringify(req.body[i]);
+                    var con = JSON.parse(js);
+                    if (con.weight > 0) {
+                        queryArray.push(con.food_index);
+                        queryArray.push(con.weight);
+                        queryArrayTmp.push(con.food_index);
+                    }
+                    if (con.weight > 0) {
+
+                        connection.query('UPDATE user_info SET weight = weight + ?  WHERE user_id = ? and food_index = ? ; ', [con.weight, con.user_id, con.food_index], function (error2, info2) {
                             if (error2 != null) {
                                 console.log(error2);
                                 res.status(503).json(error);
                             }
                         });
 
-                        connection.query('UPDATE my_info SET my_weight = my_weight + ? WHERE user_id = ? and my_food_index = ? and my_distinction = ?; ', [con.weight, con.user_id, con.food_index, con.distinction], function (error2, info2) {
-                            if (error2 != null) {
-                                console.log(error2);
-                                res.status(503).json(error);
-                            }
-                        });
-
-
-                        connection.query('UPDATE food_list SET rank = rank + ? WHERE food_index = ? and distinction = ?; ', [con.weight, con.food_index, con.distinction], function (error2, info2) {
+                        connection.query('UPDATE food_list SET rank = rank + ? WHERE food_index = ? ; ', [con.weight, con.food_index], function (error2, info2) {
                             if (error2 != null) {
                                 console.log(error2);
                                 res.status(503).json(error);
@@ -47,42 +66,52 @@ router.post('/', function (req, res, next) {
                         });
                     }
                 }
-                else if (info1.length <= 0) {
-                    console.log('test');
-                    for (var i = 0; i < 64; i++) {  //for문시작
-                        var js = JSON.stringify(req.body[i]);
-                        var con = JSON.parse(js);
 
-                        connection.query('insert into user_info(user_id, food_index, weight, distinction) values(?,?,?,?);', [con.user_id, con.food_index, con.weight, con.distinction], function (error3, info3) {
-                            if (error3 != null) {
-                                console.log(error3);
-                                res.status(503).json(error3);
-                            }
-                        });
-
-                        connection.query('insert into my_info(user_id, my_food_index, my_weight, my_distinction) values(?,?,?,?);', [con.user_id, con.food_index, con.weight, con.distinction], function (error3, info3) {
-                            if (error3 != null) {
-                                console.log(error3);
-                                res.status(503).json(error3);
-                            }
-                        });
-
-                        connection.query('UPDATE food_list SET rank = rank + ? WHERE food_index = ? and distinction = ?; ', [con.weight, con.food_index, con.distinction], function (error3, info3) {
-                            if (error3 != null) {
-                                console.log(error3);
-                                res.status(503).json(error3);
-                            }
-                        });
+                queryArray.concat(queryArrayTmp);
+                connection.query(query, queryArray, function (error2, info2) {
+                    if (error2 != null) {
+                        console.log(error2);
+                        res.status(503).json(error);
                     }
+                });
+
+
+            }
+            else if (info1.length <= 0) {
+                console.log('test');
+                for (var i = 0; i < 64; i++) {  //for문시작
+
+
+                    var js = JSON.stringify(req.body[i]);
+                    var con = JSON.parse(js);
+
+                    connection.query('insert into user_info(user_id, food_index, weight, distinction) values(?,?,?,?);', [con.user_id, con.food_index, con.weight, con.distinction], function (error3, info3) {
+                        if (error3 != null) {
+                            console.log(error3);
+                            res.status(503).json(error3);
+                        }
+                    });
+
+                    connection.query('insert into my_info(user_id, my_food_index, my_weight, my_distinction) values(?,?,?,?);', [con.user_id, con.food_index, con.weight, con.distinction], function (error3, info3) {
+                        if (error3 != null) {
+                            console.log(error3);
+                            res.status(503).json(error3);
+                        }
+                    });
+
+                    connection.query('UPDATE food_list SET rank = rank + ? WHERE food_index = ? and distinction = ?; ', [con.weight, con.food_index, con.distinction], function (error3, info3) {
+                        if (error3 != null) {
+                            console.log(error3);
+                            res.status(503).json(error3);
+                        }
+                    });
                 }
             }
-            else {
-                console.log(error);
-                res.status(503).json(error);
-            }
-        });
-    } catch (e) {
-        console.log(e);
-    }
+        }
+        else {
+            console.log(error);
+            res.status(503).json(error);
+        }
+    });
 });
 module.exports = router;
